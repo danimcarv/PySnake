f0a53dcfa3bd0383f836b39cdb127b13dc34ab47
danimont228 <danimont228@gmail.com>
2024-02-04 11:04:58 -0300

    Added scenario and moving mechanisms, also added column collision treatment

--- game.py
+++ game.py
@@ -1,52 +1,91 @@
 import numpy as np
+import time
+import os
 
 tela = np.zeros((20,20))
+snake = []
 
-#def preencheMatriz(matriz, dimensao) :
-#    for i in range(dimensao) :
-#        for j in range(dimensao) :
-#            matr
+def zeraMatriz() :
+    for i in range(len(tela)) :
+        for j in range(len(tela)) :
+            tela[i,j]=0
 
-#def criaTela(matriz, dimensao) :
+def clearScreen() :
+    if os.name=='nt' :
+        os.system('cls')
+    else :
+        os.system('clear')
+
+def redefineTela() :
+    for i in range(20) :
+        tela[i, 0] = 1
     
-#    for coluna in range(dimensao-1) :
-#        for linha in range(dimensao-1) :
-#            if coluna == linha :
-#                matriz[coluna][linha] = 1
+    for i in range(20) :
+        tela[0, i] = 1
 
-#        matriz[index][0] = 1
-#        matriz[index][(dimensao-1)] = 1
-#        matriz[0][index] = 1
-#        matriz[(dimensao-1)][index] = 1
+    for i in range(20) :
+        tela[i, 19] = 1
 
-#criaTela(tela, 20)
+    for i in range(20) :
+        tela[19, i] = 1
 
-for i in range(20) :
-    tela[i, 0] = 1
+def exibeTela() :
+    for i in range(20) :
+        print("")
+        for j in range(20) :
+            if tela[i, j] == 1 :
+                print("#", end=" ")
+            elif tela[i, j] == 0 :
+                print(" ", end=" ")
+            elif tela[i, j] == 2 :
+                print("█", end=" ")
+            else :
+                print("E", end=" ")
+    print("\n")
 
-for i in range(20) :
-    tela[0, i] = 1
+snake.append(9)
+snake.append(9)
 
-for i in range(20) :
-    tela[i, 19] = 1
-
-for i in range(20) :
-    tela[19, i] = 1
-
-print(tela)
-
-print("\n\n")
-
-for i in range(20) :
-    print(" ")
-    for j in range(20) :
-        if tela[i, j] == 1 :
-            print("#", end="")
-        elif tela[i, j] == 0 :
-            print(" ", end="")
+def snakeFill() :
+    for i in range(0,len(snake),2) :
+        x = snake[i]
+        y = snake[i+1]
+        if(tela[x,y]!=1) :
+            tela[x,y]=2
         else :
-            print(str(tela[i, j]), end="")
-#    print("\n")
+            moving = False
+            clearScreen()
+            return("Collision")
 
+direction = "right"
+moving = True
 
+def movimentaSnake() :
+    if direction == "down" :
+        for i in range(0,len(snake),2) :
+            snake[i] = snake[i]+1
+    if direction == "right" :
+        for i in range(1,len(snake),2) :
+            snake[i] = snake[i]+1
+    if direction == "up" :
+        for i in range(0,len(snake),2) :
+            snake[i] = snake[i]-1
+    if direction == "left" :
+        for i in range(1,len(snake),2) :
+            snake[i] = snake[i]-1
 
+def snakeMoving() :
+    clearScreen()
+    while(moving==True) :
+        zeraMatriz()
+        redefineTela()
+        if (snakeFill()) == "Collision" :
+            clearScreen()
+            print("GAME OVER")
+            break
+        exibeTela()
+        movimentaSnake()
+        time.sleep(1)
+        clearScreen()
+    
+snakeMoving()
